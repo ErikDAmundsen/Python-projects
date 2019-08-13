0d78354e133644d5097f2d4be38d5fe896d600e0
erikdamundsen <erikdamundsen@gmail.com>
2019-08-13 11:15:59 -0700

    ifthen

---
+++ if then/ifthen.py
@@ -0,0 +1,28 @@
+# age = int(input("How old are you"))
+# if (age>= 16) and (age<=65):
+#
+#     print("Have a good day at work")
+#
+#     if (age<16 or age>65):
+#         print ("Print enjoy your free time")
+#     else:
+#         print("Have a good day")
+y = "false"
+if y:
+    print("X is true")
+x= input("enter text")
+if x:
+    print("You entered {}".format(x))
+else:
+    print("You entered nothing")
+
+print (not False)
+print(not True)
+parrot ="norwegian blue"
+letter = input("Enter a character: ")
+if letter in parrot:
+    print("Give me an {}, Bob".format(letter))
+else:
+    print("Nope")
+
+
