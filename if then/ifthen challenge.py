0ba3a9fe9bd5e3b427ecb78faec410848fb813c8
erikdamundsen <erikdamundsen@gmail.com>
2019-08-13 11:30:25 -0700

    ifthen challenge complete

--- if then/ifthen challenge.py
+++ if then/ifthen challenge.py
@@ -1,9 +1,17 @@
-x = int(5 )
-y= int(7)
+# x = int(5 )
+# y= int(7)
+#
+# if x>y:
+#     print("x is greater than y")
+# elif x<y:
+#     print("x is smaller than y")
+# else:
+#     print("x equals y")
 
-if x>y:
-    print("x is greater than y")
-elif x<y:
-    print("x is smaller than y")
+age = int(input("What is your age"))
+name = (input("What is your name"))
+
+if age>18 and age<31:
+    print("{} you are elegible for the trip".format(name))
 else:
-    print("x equals y")
+    print("sorry {}you are ineligible for the trip".format(name))
