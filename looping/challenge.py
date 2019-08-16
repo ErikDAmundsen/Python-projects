0625258a7845115ae319d9a3871b73892a3ff854
erikdamundsen <erikdamundsen@gmail.com>
2019-08-16 14:18:15 -0700

    challenge 2 complete

--- looping/challenge.py
+++ looping/challenge.py
@@ -1,5 +1,14 @@
-inp = input("Enter your IP Address")
+import random
+highest = 10
+answer = random.randint(1,highest)
 
-segs  = 0
-count= 0
-
+print("Please guess a number btw 1 and {} ".format(highest))
+guess=0
+while guess != answer:
+    guess = int(input())
+    if guess<answer:
+        print("guess higher")
+    elif guess >answer:
+        print("guess lower")
+    else:
+     print("you got it!")
