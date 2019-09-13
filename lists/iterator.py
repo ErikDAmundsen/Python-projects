b520ae150d43cc883d6431cf2850124ca751963a
erikdamundsen <erikdamundsen@gmail.com>
2019-09-13 16:39:25 -0700

    Game 1

--- lists/iterator.py
+++ lists/iterator.py
@@ -1,2 +1,24 @@
+# string = "1234567890"
+#
+#
+# # for char in string:
+# #     print(char)
+# myIterator = iter(string)
+#
+# print(myIterator)
+# print(next(myIterator))
+# print(next(myIterator))
+#
+# flavors = ["cherry", "chocole", "vanilla"]
+# MyIt = iter(flavors)
+#
+# for MyIt in range(len(flavors)):
+#     print(next (MyIt))
 
+# x = range(1000,10,-10)
+# for L in x:
+#     print(L)
 
+st="Erik Amundsen"
+print(st[1])
+print(st.index("A"))
